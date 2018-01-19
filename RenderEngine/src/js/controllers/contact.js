190354a4fd5edbbdd5c114bebc96e3b7c3f0c9be
Edwin Hauspie <edwin.hauspie@novado.be>
2018-01-19 13:54:53 +0100

    Arbitrary changes / Clean up

--- RenderEngine/src/js/controllers/contact.js
+++ RenderEngine/src/js/controllers/contact.js
@@ -3,10 +3,14 @@ import BaseController from './_base'
 class ContactController extends BaseController {
     constructor(controllerConfig) {
         super(controllerConfig, 'contact')
-        BaseController.call(this, controllerConfig, 'contact') ///////IE10 fix
+
+        //IE10 fix: super not working correctly in constructor with babel
+        //Note: in browsers without this issue, the constructor will get fired twice
+        BaseController.call(this, controllerConfig, 'contact')
 
         this.models.index = {
-            ...controllerConfig
+            ...controllerConfig,
+            text: 'Brussels'
         }
     }
 
--- RenderEngine/src/js/controllers/home.js
+++ RenderEngine/src/js/controllers/home.js
@@ -1,27 +1,31 @@
 import BaseController from './_base'
 
+function getRandom(min, max) {
+    return Math.floor(Math.random() * max) + min
+}
+
 class HomeController extends BaseController {
     constructor(controllerConfig) {
         super(controllerConfig, 'home')
-        BaseController.call(this, controllerConfig, 'home') ///////IE10 fix
+
+        //IE10 fix: super not working correctly in constructor with babel
+        //Note: in browsers without this issue, the constructor will get fired twice
+        BaseController.call(this, controllerConfig, 'home')
 
         let root = this
 
         this.models.index = {
             ...controllerConfig,
-            getRnd: function(min, max) {
-                return Math.floor(Math.random() * max) + min
-            },
             number: 123,
             refreshNumber: function () {
-                this.number = this.getRnd(1, 1000)
+                this.number = getRandom(1, 1000)
                 root.render('index', '#number')
             }
         }
 
         this.models.about = {
             ...controllerConfig,
-            names: ['An', 'Jan', 'Rik', 'Leen'],
+            names: ['An', 'Jan', 'Rik'],
             addName: function (sender, e) {
                 if (!sender.value.trim()) return
                 this.names.push(sender.value.trim())
