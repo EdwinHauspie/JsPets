3b5f04a96b220c0b96f7a3c0179ce5ab40df30c0
Edwin Hauspie <edwin.hauspie@novado.be>
2018-01-30 16:01:24 +0100

    Jit complilation of views

--- RenderEngine/src/js/controllers/_base.js
+++ RenderEngine/src/js/controllers/_base.js
@@ -3,14 +3,13 @@ import LittleEngine from '../little-engine'
 class BaseController {
     constructor(controllerConfig, views) {
         this.config = controllerConfig
+        this.views = views
         this.models = {}
-
-        this.views = {}
-        Object.keys(views).forEach(v => this.views[v] = LittleEngine.createRenderer(views[v]))
     }
 
     execute(actionName, routeParams) {
-        if (!this[actionName]) throw Error(`Action "${actionName}" not found. Check controller or router config.`)
+        if (!this[actionName])
+            throw Error(`Action "${actionName}" not found. Check controller or router config.`)
 
         //Do action and bind model to the main element to catch events
         $('.js-main')[0].viewModel = this.models[actionName]
@@ -18,13 +17,18 @@ class BaseController {
     }
 
     render(actionName, selector) {
+        //Compile view if necessary
+        if (typeof this.views[actionName] === 'string')
+            this.views[actionName] = LittleEngine.createRenderer(this.views[actionName])
+
+        //Render html
         let html = LittleEngine.render(this.views[actionName], this.models[actionName])
 
         if (selector) {
             let vDom = document.createElement('div')
             vDom.innerHTML = html
             var newPart = $(selector, vDom)
-            var oldPart = $(selector)
+            var oldPart = $(selector, '.js-main')
             oldPart.replaceWith(newPart)
         }
         else $('.js-main').html(html)
