132f02aa9c73c277663ec33b04e25f3778570b1d
Edwin Hauspie <edwin.hauspie@novado.be>
2018-02-20 14:58:54 +0100

    Fixed rewrite in loader

--- RenderEngine/view-loader.js
+++ RenderEngine/view-loader.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
-/*function toCamelCase(str) {
+function toCamel(str) {
     return str.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); })
-}*/
+}
 
 module.exports = function (content) {
     this.cacheable && this.cacheable();
@@ -10,8 +10,7 @@ module.exports = function (content) {
     let srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/');
 
     content = `import { html } from '${srcPath}/js/helpers.js'\n` + content
-        //.replace(/<c-([^>]+)>(.*)<\/c-([^>]+)>/gi, '${$1' + '($2)}') //Replace <c-xxx>
-        .replace(/<c-([^>\s]+)(.+)\/>/gi, function(str, $1, $2) { return '${' + $1 + '({' + $2.replace(/([^=]+)="([^"]+)"/gi, '$1: $2') + '})}' }) //Replace <c-xxx>
+        .replace(/<c-([^>\s]+)(.+)\/>/gi, function(str, $1, $2) { return '${' + toCamel($1) + '({' + $2.replace(/([^=]+)="([^"]+)"/gi, '$1: $2, ') + '})}' }) //Replace <c-xxx>
         .replace(/<for each="([^"]+)" in="([^"]+)">/gi, '${(Number.parseInt($2) ? (Array.from(Array($2)).map((x, i) => i+1)) : $2).map(($1) => html`') //Replace <for>
         .replace(/<\/for>/gi, '`)}') //Replace </for>
 
